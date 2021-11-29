dc753c18f5e7252c1e7d438aa12f456b1e68408b
Maria Kushlianskaya <mariakazakova92@gmail.com>
2021-11-29 13:41:29 +0300

    refactor functional component Users to class component Users

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,16 +3,19 @@ import axios from "axios";
 import styles from "./users.module.css";
 import userPhoto from "../../assets/user.png"
 
-let Users = (props) => {
-    if (props.users.length === 0) {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                    props.setUsers(response.data.items)
-                })
+            this.props.setUsers(response.data.items)
+        })
     }
 
-    return <div>
-                {
-                    props.users.map(u => <div key={u.id}>
+    render =() => {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                         <span>
                                 <div >
                                     <img src={u.photos.small !== null ?  u.photos.small : userPhoto} className={styles.photo}/>
@@ -20,10 +23,10 @@ let Users = (props) => {
                                 <div>
                                     {u.followed
                                         ? <button onClick={() => {
-                                            props.unfollow(u.id)
+                                            this.props.unfollow(u.id)
                                         }}>Unfollow</button>
                                         : <button onClick={() => {
-                                            props.follow(u.id)
+                                            this.props.follow(u.id)
                                         }}>Follow</button>}
 
                                 </div>
@@ -31,7 +34,7 @@ let Users = (props) => {
                             </span>
 
 
-                        <span>
+                    <span>
                                 <span>
                                     <div>{u.name}</div>
                                     <div>{u.status}</div>
@@ -42,10 +45,10 @@ let Users = (props) => {
                                 </span>
 
                             </span>
-                    </div>)
-                }
-           </div>
-
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Users from "./Users";
 import {followAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
 import { connect } from 'react-redux';
+import Users from "./Users";
 
 
 let mapStateToProps = (state) => {
