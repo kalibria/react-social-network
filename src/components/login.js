dab1ff23e52856825886e58e75ce62af41c327a4
Maria Kushlianskaya <mariakazakova92@gmail.com>
2021-12-16 14:03:26 +0300

    start creat markup of login page

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Login = () => {
-    return <h1>Login</h1>
+    return <div>
+        <h1>Login</h1>
+        <div>
+            <input placeholder={"Login"}/>
+        </div>
+        <div>
+            <input placeholder={"Password"}/>
+        </div>
+        <div>
+            <input type={"checbox"} />remember me
+        </div>
+        <div>
+            <button>Login</button>
+        </div>
+    </div>
 }
 
 export default Login
